9260ad09e0baa98f709abceeb980e4689198360a
Gustavo Soares <gustavohvs.dev@gmail.com>
2023-05-22 17:10:12 -0300

    tentativa de configuracao do xdebug

--- src/vendor/composer/autoload_static.php
+++ src/vendor/composer/autoload_static.php
@@ -37,8 +37,8 @@ class ComposerStaticInit25eace8fd792cfa24d5eca43c200726b
     public static $prefixDirsPsr4 = array (
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
--- src/vendor/composer/installed.php
+++ src/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3da6efc4419b27aa4e790eb8cd10591cc5728429',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3da6efc4419b27aa4e790eb8cd10591cc5728429',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
