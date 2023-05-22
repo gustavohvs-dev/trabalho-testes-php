f47e0f85534f674242cc84e5aa2ff39ab79b739a
matsuryuji <mrmatsutane@hotmail.com>
2023-05-22 14:19:23 -0300

    test login

--- src/tests/LoginTest.php
+++ src/tests/LoginTest.php
@@ -1,2 +1,34 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
+use app\Api;
+
+class ApiTest extends TestCase
+{
+    public function testWsmethodReturnsCorrectResponse()
+    {
+        // Configuração
+        $systemConfig = [
+            "api" => [
+                "token" => "your_secret_token"
+            ]
+        ];
+
+        $api = new Api($systemConfig);
+
+        // Dados de entrada
+        $data = (object) [
+            "username" => "your_username",
+            "password" => "your_password"
+        ];
+
+        // Execução do método sendo testado
+        $response = $api->wsmethod($data);
+
+        // Verificações/assertions
+        $this->assertEquals("200 OK", $response->code);
+        $this->assertEquals(1, $response->response["status"]);
+        $this->assertEquals(200, $response->response["code"]);
+        $this->assertNotEmpty($response->response["token"]);
+    }
+}
