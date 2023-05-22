9b586cd1d8b158c77954cbf51d6456aafbdee13d
Gustavo Soares <gustavohvs.dev@gmail.com>
2023-05-22 17:48:25 -0300

    update mysql file

--- src/readme/trabalhotestes.sql
+++ src/readme/trabalhotestes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 22-Maio-2023 às 16:51
+-- Tempo de geração: 22-Maio-2023 às 22:46
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 7.4.33
 
@@ -46,6 +46,27 @@ INSERT INTO `comments` (`id`, `fk_topic`, `fk_user`, `comment`, `createdAt`, `up
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `improvementsuggestions`
+--
+
+CREATE TABLE `improvementsuggestions` (
+  `id` int(11) NOT NULL,
+  `fk_user` int(11) NOT NULL,
+  `suggestion` text NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `improvementsuggestions`
+--
+
+INSERT INTO `improvementsuggestions` (`id`, `fk_user`, `suggestion`, `createdAt`, `updatedAt`) VALUES
+(1, 1, 'shmujk183m', '2023-05-22 20:38:05', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `topics`
 --
 
@@ -63,7 +84,11 @@ CREATE TABLE `topics` (
 --
 
 INSERT INTO `topics` (`id`, `title`, `description`, `keywords`, `createdAt`, `updatedAt`) VALUES
-(1, 'TOPIC 1', 'TESTE DESCRIPTION', 'TESTE;TESTE2', '2023-05-22 14:50:39', NULL);
+(1, 'TOPIC 1', 'TESTE DESCRIPTION', 'TESTE;TESTE2', '2023-05-22 14:50:39', NULL),
+(2, '111', '123', '123', '2023-05-22 18:43:56', NULL),
+(3, 'h5nspnecdy', '90ez6fgpff', 'nw8pm0fg0i', '2023-05-22 18:44:10', NULL),
+(4, '3j4ukw96eo', 'xeionvlxor', '76qqd5bezw', '2023-05-22 18:45:14', NULL),
+(5, '4kfge5vaz9', 'pkdla0iw6t', 'akpv1zre38', '2023-05-22 18:46:25', NULL);
 
 -- --------------------------------------------------------
 
@@ -85,7 +110,13 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `permission`, `createdAt`, `updatedAt`) VALUES
-(1, 'admin', '$2y$10$OYB7Qe02EZvv6S8NeWbqDuUF7bxv2NvHDhNpEFZOe7JC1LPz8Wp0y', 'admin', '2023-05-22 00:49:23', NULL);
+(1, 'admin', '$2y$10$OYB7Qe02EZvv6S8NeWbqDuUF7bxv2NvHDhNpEFZOe7JC1LPz8Wp0y', 'admin', '2023-05-22 00:49:23', NULL),
+(49, '4dfbex92qq', '$2y$10$RVLmjXuuccQAo.9KlWTj1.PEh/fWY0NIbImTAyFUPhzeHEhRADG5G', 'supervisor', '2023-05-22 15:19:30', NULL),
+(50, 'gustavosoares', '$2y$10$0UPlcThMY1X2gXfL26H6g.1ELsdRY5ArMqxp19gxMwYDnqwmkkGT6', 'supervisor', '2023-05-22 15:19:30', NULL),
+(51, '6jemejyih1', '$2y$10$HvGe0WSbYOUlcd.pEBxlbenHJiBf/vw.glFAdiJabwWQPjz0Np6DW', 'supervisor', '2023-05-22 15:24:33', NULL),
+(53, 'hi7ubzquq7', '$2y$10$CCmyA7Im3X4AFaTWaRRG5OGmGnBjn7p0pJpcgg/AJ78GPwTmNfPRi', 'supervisor', '2023-05-22 15:24:53', NULL),
+(55, 'gustavo7', '$2y$10$u1fhFmiTErcO.so0M60KPuCsGb7MI8otp6ZhDCTDLWbXjClDjoIGm', 'common', '2023-05-22 15:49:26', NULL),
+(56, 'sa9ad3fwvo', '$2y$10$2IB..yoAq7ZstKlSqhYuPemOhGEC0GtuuXHHHNAUpXvQFaX52RVPS', 'supervisor', '2023-05-22 16:02:15', '2023-05-22 13:02:16');
 
 --
 -- Índices para tabelas despejadas
@@ -98,6 +129,13 @@ ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_user` (`fk_user`),
   ADD KEY `fk_topic` (`fk_topic`);
+
+--
+-- Índices para tabela `improvementsuggestions`
+--
+ALTER TABLE `improvementsuggestions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user` (`fk_user`);
 
 --
 -- Índices para tabela `topics`
@@ -120,19 +158,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT de tabela `improvementsuggestions`
+--
+ALTER TABLE `improvementsuggestions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `topics`
 --
 ALTER TABLE `topics`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=159;
 
 --
 -- Restrições para despejos de tabelas
@@ -144,6 +188,12 @@ ALTER TABLE `users`
 ALTER TABLE `comments`
   ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`fk_user`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`fk_topic`) REFERENCES `topics` (`id`);
+
+--
+-- Limitadores para a tabela `improvementsuggestions`
+--
+ALTER TABLE `improvementsuggestions`
+  ADD CONSTRAINT `improvementsuggestions_ibfk_1` FOREIGN KEY (`fk_user`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
