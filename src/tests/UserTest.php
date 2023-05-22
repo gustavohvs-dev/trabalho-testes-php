c5f9e86b930508c5eb4232e97196282d511b28b5
Gustavo Soares <gustavohvs.dev@gmail.com>
2023-05-21 22:47:16 -0300

    test duplicated user

--- src/tests/UserTest.php
+++ src/tests/UserTest.php
@@ -4,6 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+    //Pendente - automatizar coleta de token e id
     private $http;
     private $token = "eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCJ9.eyJzdWIiOiAiMSIsICJuYW1lIjogImFkbWluIiwgImlhdCI6IDE2ODQ3MTY3MDUsICJleHAiOiAxNjg0ODAzMTA1LCJpZCI6ICIxIiwidXNlcm5hbWUiOiAiYWRtaW4ifQ.CHBO8PLj1E80DarPkY8V3UEWVIBEEC4i0iDvpOGWboU";
 
@@ -18,7 +19,7 @@ class UserTest extends TestCase
     }
 
     /**
-     * Verify if the server is working normally
+     * /createUser - Testa o cadastro de um novo usuário 
      */
     public function testCreateUserSuccesfully()
     {
@@ -35,9 +36,32 @@ class UserTest extends TestCase
         ]);
         $this->assertEquals(200, $response->getStatusCode());
         $body = json_decode($response->getBody()->getContents());
-        var_dump($body);
+        //var_dump($body);
         $this->assertEquals(200, $body->code);
         $this->assertEquals("Registro criado com sucesso", $body->info);
+    }
+
+    /**
+     * /createUser - Testa o cadastro de um usuário repetido
+     */
+    public function testCreateUserDuplicated()
+    {
+        
+        require "config.php";
+        $data = [
+            'username' => 'gustavosoares',
+            'permission' => 'supervisor',
+            'password' => '189374983274'
+        ];
+        $response = $this->http->post($config['path']."createUsers", [
+            'headers' => [ 'Authorization' => 'Bearer ' . $this->token ],
+            'body' => json_encode($data)
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $body = json_decode($response->getBody()->getContents());
+        //var_dump($body);
+        $this->assertEquals(500, $body->code);
+        $this->assertEquals("Falha ao criar registro", $body->info);
     }
 
 }
